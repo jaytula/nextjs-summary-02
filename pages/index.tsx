6fce943dbaf9d104e7bb67cfb8458c47de8dbdd1
Jay T <jaytula@gmail.com>
2021-05-03 15:37:10 -0700

    254. Introducing Datga Fetching For Page Generation (getStaticProps)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,35 @@
-import { useEffect, useState } from "react";
+import { GetStaticProps } from "next";
 import MeetupList, { IMeetup } from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS: IMeetup[] = [
   {
-    id: 'm1',
-    title: 'A First Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG/1280px-Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG',
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!',
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG/1280px-Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
   },
   {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG/1280px-Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG',
-    address: 'Some address 10, 12345 Some City',
-    description: 'This is a second meetup!',
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG/1280px-Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG",
+    address: "Some address 10, 12345 Some City",
+    description: "This is a second meetup!",
   },
-]
+];
 
-const HomePage = () => {
-  const [loadedMeetups, setLoadedMeetups] = useState<IMeetup[]>([])
-  useEffect(() => {
-    setLoadedMeetups(DUMMY_MEETUPS);
-  }, [])
-  return <MeetupList meetups={loadedMeetups} />;
+const HomePage = ({ meetups }: { meetups: IMeetup[] }) => {
+  return <MeetupList meetups={meetups} />;
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
 };
 
 export default HomePage;
