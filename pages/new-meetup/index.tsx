1c1427ca6e43b9cc62d27825bc57ea9f8ac6fe1a
Jay T <jaytula@gmail.com>
2021-05-04 00:46:46 -0700

    261. Sending HTTP Requests To API Routes

--- pages/new-meetup/index.tsx
+++ pages/new-meetup/index.tsx
@@ -1,11 +1,24 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import {useRouter} from 'next/router'
 
 function NewMeetupPage() {
-  const addMeetupHandler = (enteredMeetupData) => {
-    console.log(enteredMeetupData)
-  }
+  const router = useRouter();
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+  const addMeetupHandler = async (enteredMeetupData) => {
+    const res = await fetch("/api/new-meetup", {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(enteredMeetupData),
+    })
+
+    const data = await res.json();
+    console.log(data);
+    router.push('/');
+  };
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 }
 
 export default NewMeetupPage;
