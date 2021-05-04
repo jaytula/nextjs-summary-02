a7a5ca33ba5109bdb5e2453af6ff493f172e3162
Jay T <jaytula@gmail.com>
2021-05-04 00:29:45 -0700

    260. Connecting & Querying a MongoDB Database

--- pages/api/new-meetup.ts
+++ pages/api/new-meetup.ts
@@ -1,10 +1,30 @@
 import { NextApiHandler } from "next";
+import { MongoClient } from "mongodb";
 
-const handler: NextApiHandler = (req, res) => {
-  if (req.method === "POST") {
-    const data = req.body;
-    const { title, image, address, description } = data;
+const handler: NextApiHandler = async (req, res) => {
+  if (req.method !== "POST") {
+    return;
   }
+
+  const data = req.body;
+  const { title, image, address, description } = data;
+
+  const client = await MongoClient.connect(process.env.MONGO_URI as string);
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const newMeetup = {
+    title,
+    image,
+    address,
+    description,
+  };
+  const result = await meetupsCollection.insertOne(newMeetup);
+  console.log(result);
+  client.close();
+
+  res.status(201).json({
+    message: "Meetup inserted!",
+  });
 };
 
 export default handler;
